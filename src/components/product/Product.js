db9b0d16c53ef54f6a2dca07b2bef4149bab26c8
NgatiaJeffers <jeffersongakuya@gmail.com>
2022-02-02 18:16:21 +0300

    Handle click event

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -11,7 +11,7 @@ class Product extends React.Component {
     render() {
         let {
             products,
-            handleProductUpVote
+            onVote
         } = this.props;
 
         return (
@@ -30,7 +30,7 @@ class Product extends React.Component {
                                         <dd className="w-64 bg-lime-200 truncate">{item.description}</dd>
                                     </div>
                                     <div>
-                                        <dd className="flex items-center" onClick={this.handleUpVote}>
+                                        <dd className="flex items-center cursor-pointer" onClick={onVote}>
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" />
                                             </svg>
--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -5,15 +5,15 @@ import { products } from "../../data/seed";
 // Parent component
 
 class ProductList extends React.Component {
-    handleProductupVote(products) {
-        console.log(productsId + ' was upvoted');
+    handleProductUpVote = (products) => {
+        console.log(products.id + ' was upvoted.')
     }
 
     render() {
         return (
             <div className="content-center divide-y divide-slate-200">
                 <h1 className="font-bold text-center text-3xl text-lime-500 m-5">Popular Products</h1>
-                <Product products={products} handleProductUpVote />
+                <Product products={products} onVote={this.handleProductUpVote} />
             </div>
         )
     }
